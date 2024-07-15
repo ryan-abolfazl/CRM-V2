12501f67ee66b3a83047e65c2f3a935394990b64
Ryan <ryan.abolfazl@gmail.com>
2024-07-15 22:01:08 +0330

    initial commit

---
+++ accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+CustomUserModel = get_user_model()
+
+# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = (
+        'email',
+        'username',
+        'is_superuser',
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
