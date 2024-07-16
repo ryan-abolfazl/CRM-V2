6398d2331eb33f42b93b3110ec0364628fec6688
Ryan <ryan.abolfazl@gmail.com>
2024-07-17 00:01:51 +0330

    templates & tailwindcss

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from leads.views import landing_page, LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='landing-page'),
+    path('leads/',  include('leads.urls', namespace="leads"))
 ]
--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,24 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.conf import settings
 # Create your models here.
+# CustomUser = get_user_model()
 
 
-class Leads(models.Model):
+class Agent(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user)
+
+class Lead(models.Model):
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name or ''
+
+
